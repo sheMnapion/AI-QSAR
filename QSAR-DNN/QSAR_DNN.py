f68cfd8337dc9ea0e6c21d3411a069d755b4e240
ruiqigao <1210077937@qq.com>
2019-12-24 20:18:26 +0800

    regulaize data

--- QSAR-DNN/QSAR_DNN.py
+++ QSAR-DNN/QSAR_DNN.py
@@ -9,6 +9,8 @@ from torchvision import datasets, transforms
 from sklearn.metrics import r2_score 
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import pyqtSignal
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 
 #deep neural network: in_dim,n_hidden_1,n_hidden_2,n_hidden_3,out_dim
@@ -61,9 +63,8 @@ class QSARDNN():
 
         progress_callback.emit('\n***********Start Training***********\n')
 
-        train_len = int(train_set.shape[0]/batch_size)*batch_size
-        train_set = train_set[:train_len]
-        train_label=train_label[:train_len]
+        train_set,va_set,train_label,va_label = train_test_split(train_set,train_label,test_size = 0.2,random_state = 0)
+        train_set = preprocessing.scale(train_set,axis=0)
 #        print(train_len)
 #        print(train_set.shape,train_label.shape)
         # exit(0)
@@ -77,13 +78,16 @@ class QSARDNN():
             #use CrossEntropy loss function for classification
             self.loss_func = nn.CrossEntropyLoss() 
         #Create Gradient Descent Optimizer
-        self.optimizer = torch.optim.SGD(self.model.parameters(), learning_rate)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), learning_rate)
         num_nongrowth = 0
 
         for epoch in range(num_epoches):
             avg_loss = 0
             for i, data in enumerate(train_loader, start=0):
+                self.model.train()
                 train, label = data
+                if train.shape[0] < batch_size:
+                    continue
                 # print('train and label shapes:',train.shape,label.shape)
                 train = train.float()
                 if self.dnn_type == 0:
@@ -96,16 +100,17 @@ class QSARDNN():
                     loss = self.loss_func(pred,label)
                 else:
                     loss = self.loss_func(pred,label.squeeze())    
-                avg_loss = avg_loss + (loss.data.numpy()/len(train_loader))
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
             
+            va_pred = self.test(va_set,va_label)
+            avg_loss = r2_score(va_pred,va_label)
             self.loss_list.append(avg_loss)
             if epoch == 0:
                 old_loss = avg_loss
             else:
-                if avg_loss < old_loss:
+                if avg_loss > old_loss:
                     num_nongrowth = 0
                     old_loss = avg_loss
                 else:
@@ -119,7 +124,7 @@ class QSARDNN():
                 # print('loss : {}'.format(avg_loss))
                 progress_callback.emit('epoch [{}/{}]'.format(epoch + 10, num_epoches))
                 progress_callback.emit('*' * 10)
-                progress_callback.emit('loss : {}'.format(avg_loss))
+                progress_callback.emit('validation R2 score : {}'.format(avg_loss))
 
         progress_callback.emit('\n***********Finish Training***********\n')
 
@@ -128,8 +133,9 @@ class QSARDNN():
     
     #test_set must be 2d --- data_num*property_num
     def test(self,test_set,test_label):
-        #self.model.eval()
+        self.model.eval()
         single_input = 0
+        test_set = preprocessing.scale(test_set,axis=0)
         #batch normalization can not handle single input
         if test_set.shape[0] == 1:
             test_set=np.array([test_set[0],test_set[0]])
@@ -154,15 +160,17 @@ class QSARDNN():
 
 
 if __name__ == '__main__':
-    csv_data = pd.read_csv("../datasets/delaney-processed.csv")#location of the data file
+    #csv_data = pd.read_csv("../datasets/delaney-dropped_transformed.csv")#location of the data file
+    csv_data = pd.read_csv("../datasets/desc_canvas_aug30_transformed.csv")
     columns = csv_data.columns
     device = torch.device('cpu')
     a = []
     label = []
     for i in columns:
-        #i != "ESOL predicted log solubility in mols per litre" and
-        if  i != "Compound ID" and i != "smiles":
-            if i == "measured log solubility in mols per litre":
+        #if  i != "ESOL predicted log solubility in mols per litre" and i != "Compound ID" and i != "smiles":
+        #    if i == "measured log solubility in mols per litre":
+        if  i != "mol" and i != "CID" and i != "Class" and i != "Model":
+            if i == "pIC50":
                 label = csv_data[i].to_list()
             else:
                 a.append(csv_data[i].to_list())
@@ -170,28 +178,24 @@ if __name__ == '__main__':
     data_set = np.array(a).T
     property_num = data_set.shape[1]
 
-    train_len = 1000
-    test_len = data_set.shape[0] - train_len
     #divide training set and test set
-    train_set = data_set[0:train_len:1]
-    test_set = data_set[train_len:data_set.shape[0]:1]
+    
     data_label = np.array(label).reshape(len(label),1)
-    train_label = data_label[0:train_len:1]
-    test_label = data_label[train_len:data_set.shape[0]:1]
+    train_set,test_set,train_label,test_label = train_test_split(data_set,data_label,test_size = 0.2,random_state = 0)
 
     batch_size = 50
     learning_rate = 0.01
     num_epoches = 1000
 
     model = QSARDNN(0,property_num)
-    num_epoches , loss_list = model.train(train_set,train_label,batch_size,learning_rate,num_epoches,0,50)
+    num_epoches , loss_list = model.train(train_set,train_label,batch_size,learning_rate,num_epoches,1,20)
     pred = model.test(test_set,test_label)
     print('R2 score: {}'.format(r2_score(pred,test_label)))
 
     #draw R2 score and L2 loss curve
-    x1 = range(0,num_epoches)
-    y1 = loss_list
-    plt.plot(x1, y1, '.-')
-    plt.title('loss vs. epoches')
-    plt.ylabel('loss')
+    x1 = pred
+    y1 = test_label
+    plt.scatter(x1, y1)
+    plt.title('pred')
+    plt.ylabel('real label')
     plt.show()
--- QSAR-DNN/_QSAR_DNN_.py
+++ QSAR-DNN/_QSAR_DNN_.py
@@ -9,7 +9,8 @@ from torchvision import datasets, transforms
 from sklearn.metrics import r2_score 
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import pyqtSignal
-
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 #deep neural network: in_dim,n_hidden_1,n_hidden_2,n_hidden_3,out_dim
 class DNN(nn.Module):
@@ -44,7 +45,7 @@ class QSARDNN():
     def __init__(self,dnn_type,property_num):
         if dnn_type != 0:
             self.dnn_type = 2
-        self.model = DNN(property_num, property_num, property_num,property_num,self.dnn_type)
+        self.model = DNN(property_num, property_num,property_num,property_num,self.dnn_type)
 
     def train(self,train_set,train_label,batch_size,learning_rate,num_epoches,early_stop,max_tolerance):
 
@@ -57,11 +58,9 @@ class QSARDNN():
 #        progress_callback.emit('max_tolerance: {}'.format(max_tolerance))
 
 #        progress_callback.emit('\n***********Start Training***********\n')
-
-        train_len = int(train_set.shape[0]/batch_size)*batch_size
-        train_set = train_set[0:train_len]
-        print(train_len)
-        print(train_set.shape)
+        train_set,va_set,train_label,va_label = train_test_split(train_set,train_label,test_size = 0.2,random_state = 0)
+        train_set = preprocessing.scale(train_set,axis=0)
+        
         #create training data loader
         train_data = QSARDataset(train_set,train_label)
         train_loader = DataLoader(train_data,batch_size=batch_size, shuffle=True)
@@ -72,13 +71,16 @@ class QSARDNN():
             #use CrossEntropy loss function for classification
             self.loss_func = nn.CrossEntropyLoss() 
         #Create Gradient Descent Optimizer
-        self.optimizer = torch.optim.SGD(self.model.parameters(), learning_rate)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), learning_rate)
         num_nongrowth = 0
 
         for epoch in range(num_epoches):
             avg_loss = 0
             for i, data in enumerate(train_loader, start=0):
+                self.model.train()
                 train, label = data
+                if train.shape[0] < batch_size:
+                    continue
                 train = train.float()
                 if self.dnn_type == 1:
                     label = label.float()
@@ -87,16 +89,16 @@ class QSARDNN():
                     loss = self.loss_func(pred,label)
                 else:
                     loss = self.loss_func(pred,label.squeeze())    
-                avg_loss = avg_loss + (loss.data.numpy()/len(train_loader))
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-            
+            va_pred = self.test(va_set,va_label)
+            avg_loss = r2_score(va_pred,va_label)
             self.loss_list.append(avg_loss)
             if epoch == 0:
                 old_loss = avg_loss
             else:
-                if avg_loss < old_loss:
+                if avg_loss > old_loss:
                     num_nongrowth = 0
                     old_loss = avg_loss
                 else:
@@ -107,7 +109,7 @@ class QSARDNN():
             if epoch % 10 == 0:
                 print('epoch [{}/{}]'.format(epoch + 10, num_epoches))
                 print('*' * 10)
-                print('loss : {}'.format(avg_loss))
+                print('validation R2 score : {}'.format(avg_loss))
 #                progress_callback.emit('epoch [{}/{}]'.format(epoch + 10, num_epoches))
 #                progress_callback.emit('*' * 10)
 #                progress_callback.emit('loss : {}'.format(avg_loss))
@@ -119,8 +121,9 @@ class QSARDNN():
     
     #test_set must be 2d --- data_num*property_num
     def test(self,test_set,test_label):
-        #self.model.eval()
+        self.model.eval()
         single_input = 0
+        test_set = preprocessing.scale(test_set,axis=0)
         #batch normalization can not handle single input
         if test_set.shape[0] == 1:
             test_set=np.array([test_set[0],test_set[0]])
@@ -145,15 +148,17 @@ class QSARDNN():
 
 
 if __name__ == '__main__':
-    csv_data = pd.read_csv("../datasets/delaney-processed.csv")#location of the data file
+    #csv_data = pd.read_csv("../datasets/delaney-dropped_transformed.csv")#location of the data file
+    csv_data = pd.read_csv("../datasets/desc_canvas_aug30_transformed.csv")
     columns = csv_data.columns
     device = torch.device('cpu')
     a = []
     label = []
     for i in columns:
-        #i != "ESOL predicted log solubility in mols per litre" and
-        if  i != "Compound ID" and i != "smiles":
-            if i == "measured log solubility in mols per litre":
+        #if  i != "ESOL predicted log solubility in mols per litre" and i != "Compound ID" and i != "smiles":
+        #    if i == "measured log solubility in mols per litre":
+        if  i != "mol" and i != "CID" and i != "Class" and i != "Model":
+            if i == "pIC50":
                 label = csv_data[i].to_list()
             else:
                 a.append(csv_data[i].to_list())
@@ -161,28 +166,24 @@ if __name__ == '__main__':
     data_set = np.array(a).T
     property_num = data_set.shape[1]
 
-    train_len = 1000
-    test_len = data_set.shape[0] - train_len
     #divide training set and test set
-    train_set = data_set[0:train_len:1]
-    test_set = data_set[train_len:data_set.shape[0]:1]
+    
     data_label = np.array(label).reshape(len(label),1)
-    train_label = data_label[0:train_len:1]
-    test_label = data_label[train_len:data_set.shape[0]:1]
+    train_set,test_set,train_label,test_label = train_test_split(data_set,data_label,test_size = 0.2,random_state = 0)
 
     batch_size = 50
     learning_rate = 0.01
     num_epoches = 1000
 
     model = QSARDNN(0,property_num)
-    num_epoches , loss_list = model.train(train_set,train_label,batch_size,learning_rate,num_epoches,0,50)
+    num_epoches , loss_list = model.train(train_set,train_label,batch_size,learning_rate,num_epoches,1,20)
     pred = model.test(test_set,test_label)
     print('R2 score: {}'.format(r2_score(pred,test_label)))
 
     #draw R2 score and L2 loss curve
-    x1 = range(0,num_epoches)
-    y1 = loss_list
-    plt.plot(x1, y1, '.-')
-    plt.title('loss vs. epoches')
-    plt.ylabel('loss')
+    x1 = pred
+    y1 = test_label
+    plt.scatter(x1, y1)
+    plt.title('pred')
+    plt.ylabel('real label')
     plt.show()
